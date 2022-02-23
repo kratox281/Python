554a308325b8fd9314eb5cd6a98469fea3aa69cb
kratox281 <rcmartinez281@gmail.com>
2022-02-23 09:13:30 +0100

    commited

---
+++ 1st Try/bucles.py
@@ -0,0 +1,12 @@
+num = 20
+
+#while num >= 1:
+    #if num%3==0:
+        #print(num)
+        #break #El break sirve para que cese el bucle en cuanto detecte esta condicion(en este caso)
+        
+   # num-=1
+    #Si quisieramos que mostrase todos los multiplos de 3 tendriamos que quitar el break
+    
+for i in range(1,6,):
+    print(i)
